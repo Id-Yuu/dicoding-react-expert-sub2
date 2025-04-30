2be3e077fd1d87ff7f2ea2ce5999971f34759517
Id-Yuu <how.we.do.9815@gmail.com>
2025-05-01 00:40:19 +0700

    make test pass

--- src/utils/setupTests.js
+++ src/utils/setupTests.js
@@ -1,4 +1,10 @@
 import '@testing-library/jest-dom';
 import { vi } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import { afterEach } from 'vitest';
 
 global.vi = vi;
+
+afterEach(() => {
+  cleanup();
+});
